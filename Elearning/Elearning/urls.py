7c51d188d2d4501b079021cba8ffef1e9899b612
AnhTuan-1206 <anhtuan20208@gmail.com>
2025-02-20 12:28:10 +0700

    Update

--- Elearning/Elearning/urls.py
+++ Elearning/Elearning/urls.py
@@ -2,7 +2,7 @@
 URL configuration for Elearning project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+        
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from django.http import HttpResponse
 urlpatterns = [
-    path('', lambda request: HttpResponse('Welcome to the Homepage!')),
     path('', include('courses.urls')),
+    path('', include('home.urls')),
     path('admin/', admin.site.urls),
+    
+    
 ]
